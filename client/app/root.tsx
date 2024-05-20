304c0a7a9d6c4e42b5e803c1f2b553dbff3cc9eb
aculaOne <strazhognay@gmail.com>
2024-05-20 22:45:47 +0400

    Add tailwind to frontend

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -5,17 +5,25 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+// eslint-disable-next-line import/no-unresolved
+import stylesheet from "~/tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      {/* Временно (стили) */}
+      <body className="h-screen bg-white dark:bg-black">
         {children}
         <ScrollRestoration />
         <Scripts />
--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -12,8 +12,16 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif" }}>
-      <h1>Добро пожаловать в Шляполяндию</h1>
+    <div className="h-full w-full flex justify-center items-center">
+      <div className="p-8 rounded-xl bg-slate-200 dark:bg-slate-800 text-center">
+        <h1 className="mb-4 text-4xl text-black dark:text-white">
+          Добро пожаловать в Шляполяндию
+        </h1>
+        <hr className="mb-8 border border-black/50 dark:border-white/50" />
+        <button className="p-2 w-full rounded bg-slate-300 dark:bg-slate-700 text-center text-black dark:text-white">
+          Авторизоваться
+        </button>
+      </div>
     </div>
   );
 }
