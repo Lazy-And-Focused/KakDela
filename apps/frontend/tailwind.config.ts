d57ec80da3c7cdbcf8c790c905c3322e4472f675
lanvalird <strazhognay@gmail.com>
2025-05-06 12:42:27 +0400

    -up depencies (снова, ведь кто-то удалил ветку с коммитами без слияния 😡)

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -1,9 +1,6 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
+  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   plugins: [],
 } satisfies Config;
