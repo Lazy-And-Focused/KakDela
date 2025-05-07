4440aec8483276c7caeba0707ad9bc88a46f71ad
lanvalird <strazhognay@gmail.com>
2025-05-07 14:55:10 +0400

    -up depencies (added prettier to frontend), new page (template)

--- apps/frontend/app/routes/_index.tsx
+++ apps/frontend/app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import {  Link, useLoaderData } from "@remix-run/react";
 import { CiLogin } from "react-icons/ci";
 
 export const meta: MetaFunction = () => {
@@ -12,7 +12,18 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader() {
+  return {
+    ENV: {
+      API_URL: process.env.API_URL,
+    },
+  };
+}
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
+
   return (
     <div className="h-full w-full flex justify-center items-center">
       <div className="p-8 flex flex-col items-center justify-center rounded-xl bg-slate-200 dark:bg-slate-800 text-center">
@@ -22,7 +33,7 @@ export default function Index() {
         <hr className="mb-8 w-full border border-black/50 dark:border-white/50" />
         <Link
           className="py-2 px-4 w-full max-w-60 rounded gap-2 flex flex-row justify-between items-center bg-slate-300 dark:bg-slate-700 text-center text-black dark:text-white"
-          to={"http://localhost:3001/api/auth/discord"}
+          to={data.ENV.API_URL + "/auth/discord"}
         >
           <p>Авторизоваться</p>
           <CiLogin size={24} />
